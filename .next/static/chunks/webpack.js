2d9ef9c54bf4f9be546289cf3e3a4dce6a7bbd97
Min-Hsuan <ylblog122@gmail.com>
2021-08-16 14:44:57 +0800

    install mongodb

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8936a23bab9170dffb6e"; }
+/******/ 		__webpack_require__.h = function() { return "fd61117abedfbfa9fedb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
