4293fabc3c68be1e5fcec2efcdc334f35ba3943c
Min-Hsuan <ylblog122@gmail.com>
2021-08-16 13:06:46 +0800

    use next.js build-in Image instead of img tag

--- components/meetups/MeetupDatail.js
+++ components/meetups/MeetupDatail.js
@@ -1,8 +1,9 @@
+import Image from 'next/image';
 import classes from './MeetupDetail.module.css';
 
 function MeetupDetail (props){
   return <div className={classes.detail}>
-    <img src={props.image} alt={props.title} />
+    <Image src={props.image} alt={props.title} />
     <h2>{props.title}</h2>
     <p>{props.address}</p>
     <p>{props.description}</p>
